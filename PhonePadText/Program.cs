971d4de55b0f05abd72102e29b5802a333b7777c
Aakash Jaiswal <aakashjaiswal@hotmail.co.in>
2025-02-14 00:28:38 +0700

    Initial Commit - Project structure

---
+++ PhonePadText/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("Hello, World!");
+
+class Solution {
+    static void Main(String[] args) {
+        
+    }
+
+    static string convertInputToCharStream(Stream input) {
+        // Creating a dictionary to map int to char in the phone
+        return "";
+    }
+    
+}

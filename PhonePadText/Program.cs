52e39d8b785b363339a7ec6928c82121584a9758
Aakash Jaiswal <aakashjaiswal@hotmail.co.in>
2025-02-14 11:44:57 +0700

    Added dictionary structure

--- PhonePadText/Program.cs
+++ PhonePadText/Program.cs
@@ -3,12 +3,24 @@
 // Console.WriteLine("Hello, World!");
 
 class Solution {
+    static Dictionary<int, List<char>> phoneNoToCharacterMap = new Dictionary<int, List<char>>
+    {
+        { 1, ['&', '\'', '('] },
+        { 2, ['A', 'B', 'C'] },
+        { 3, ['D', 'E', 'F'] },
+        { 4, ['G', 'H', 'I'] },
+        { 5, ['J', 'K', 'L'] },
+        { 6, ['M', 'N', 'O'] },
+        { 7, ['P', 'Q', 'R', 'S'] },
+        { 8, ['T', 'U', 'V'] },
+        { 9, ['W', 'X' , 'Y' , 'Z'] },
+        { 0 , ['_']}
+    };
     static void Main(String[] args) {
         
     }
 
     static string convertInputToCharStream(Stream input) {
-        // Creating a dictionary to map int to char in the phone
         return "";
     }
     
